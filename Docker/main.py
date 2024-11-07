aa6040f840b441b75a2891afa3670f8a8f4c26e3
maugendreIOD <150672573+maugendreIOD@users.noreply.github.com>
2024-11-07 10:21:58 +0100

    Test workflow

--- Docker/main.py
+++ Docker/main.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 
-# Test de test github actions test 4
+# Test de test github actions test 5
 # Charger le modèle au démarrage de l'application
 # Remplace 'model.pkl' par le chemin de ton fichier de modèle
 model = joblib.load('linear_regression_model.pkl')
