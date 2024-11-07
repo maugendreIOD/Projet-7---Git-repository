740c4393178c614d1f64dd1a2848ed469557ed28
maugendreIOD <150672573+maugendreIOD@users.noreply.github.com>
2024-11-07 23:05:29 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Test de test github actions test 12
+# Test de test github actions test 13
 
 # Détermine si nous sommes en mode test
 is_testing = os.environ.get('FLASK_TESTING') == 'true'
